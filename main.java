1a1da729777e0769ee5e88e3c205e2dcfd564c51
Rimskiy8888 <137941314+Rimskiy8888@users.noreply.github.com>
2023-07-06 23:44:32 +0200

    Homework1_Jawa_JDK11
    homework1 6.07.2023

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        String text1 = ("Hello world");
+        System.out.println(text1);
+        String text2 = ("My books");
+        System.out.println(text2);
+        String text3 = ("Roots");
+        System.out.println(text3);
+        int number1 = 250;
+        System.out.println(number1);
+        int number2 = 183;
+        System.out.println(number2);
+        int number3 = 22;
+        System.out.println(number3);
+        String text4 = (text1+number3);
+        System.out.println(text4);
+    }
+}
